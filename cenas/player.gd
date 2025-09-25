d9f49cb633fee89f91fe6b6d700dc7419f569b44
GuilhermeMAzevedo <guilhermemazevedo2007@gmail.com>
2025-09-25 17:06:14 -0300

    Aula 3

--- cenas/player.gd
+++ cenas/player.gd
@@ -10,7 +10,7 @@ enum PlayerMode{
 	SHOOTING
 }
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animated_sprite_2d = $AnimatedSprite2D as PlayerAnimatedSprite
 @onready var area_collision_shape_2d = $Area2D/AreaCollisionShape2D
 @onready var body_collision_shape_2d = $BodyCollisionShape2D
 
@@ -19,6 +19,8 @@ enum PlayerMode{
 @export var speed = 100.0
 @export var jump_velocity = -350
 @export_group("")
+
+var player_mode = PlayerMode.SMALL
 
 func _physics_process(delta):
 	
@@ -37,5 +39,7 @@ func _physics_process(delta):
 		velocity.x = lerp(velocity.x, speed * direction, run_speed_damping * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * delta)
+	
+	animated_sprite_2d.trigger_animation(velocity, direction, player_mode)
 	
 	move_and_slide()
